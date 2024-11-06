97f34a2fe272111d9db4e8375b00ab19c37afe87
Raymond Mills <rmills.gabs@gmail.com>
2024-11-06 14:27:02 -0600

    Instantiate _laserPrefab in player script at player location when space key is pressed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
     
     void Start()
     {
@@ -14,7 +16,13 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        CalculateMovement(); 
+        CalculateMovement();
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
+
     }
 
     void CalculateMovement()
