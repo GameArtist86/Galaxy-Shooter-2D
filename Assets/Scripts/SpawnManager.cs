05df00f2bd8a9841336a1bc5b99ad9f20516f69e
Raymond Mills <rmills.gabs@gmail.com>
2024-11-07 15:45:05 -0600

    Added Enemy Spawn Coroutine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine()); 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Instantiate(_enemyPrefab, new Vector3((Random.Range(-11f, 11f)), 7.5f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5);
+        }
     }
 }
