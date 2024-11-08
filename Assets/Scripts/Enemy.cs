7359661a1aae8d4f82379f128dc4ea09497bef90
Raymond Mills <rmills.gabs@gmail.com>
2024-11-07 21:24:21 -0600

    Enemy 2D sprite, enemy and player 2D rigidbody and collider, OnTriggerEnter2D script change

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -21,7 +21,7 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(Random.Range(-11f,11f), 8f, 0);
         }
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.tag == "Player") 
