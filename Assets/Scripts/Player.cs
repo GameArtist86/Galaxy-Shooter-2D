0841c668dec83a8a69acfa62609ac6e040a8b3d8
Raymond Mills <rmills.gabs@gmail.com>
2024-11-07 23:14:42 -0600

    2D Laser sprite, 2D rigidbody and collider

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -57,7 +57,7 @@ public class Player : MonoBehaviour
     void FireLaser()
     {
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.3f, 0), Quaternion.identity);
 
     }
     public void Damage()
