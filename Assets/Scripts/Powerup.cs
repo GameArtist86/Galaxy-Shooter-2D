353d294d77697d5612195fabdf50b822f775c5da
Raymond Mills <rmills.gabs@gmail.com>
2024-11-10 21:48:09 -0600

    Added _powerupID switch statement and Speed Powerup sprite

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.0f;
+    [SerializeField]
+    private int _powerupID;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (transform.position.y <= -8.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+    //OnTriggerCollision 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if (player != null)
+            {
+                switch (_powerupID)
+                {
+                    case 0:
+                        player.TripleShotCollected();
+                        break;
+                    case 1:
+                        //player.SpeedPowerupCollected();
+                        break;
+                    case 2:
+                        //player.ShieldPowerCollected();
+                        break;
+                }
+                Destroy(this.gameObject);
+
+            }
+
+            
+        }
+    }
+
+}
